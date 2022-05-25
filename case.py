9d4bba5ec4810e80cdf87c5ce8924efcdb95326c
cwzchan <cwchan0212@gmail.com>
2022-05-26 02:09:41 +0800

    Sudoku solver

--- case.py
+++ case.py
@@ -25,23 +25,85 @@ matrix = [  [5,3,0,0,7,0,0,0,0],
 
 def numOfSpace(mat):
     space = 0;
-    for i in range(len(mat)):
-        for j in range(len(mat[i])):
+    for i in range(len(mat)):           # row
+        for j in range(len(mat[i])):    # col
             if mat[i][j] == 0:
                 #print(mat[i][j])
                 space +=1
             #sq[i][j] = question[i][j]
     return space
 
-# rows = collections.defaultdict(set)
-# cols = collections.defaultdict(set)
-# squares = collections.defaultdict(set)
+def filled(mat):
+    nums = []
+    space = numOfSpace(mat)
+
+    if space > 0:
+        for i in range(len(mat)):           # row
+            for j in range(len(mat[i])):    # col
+                if mat[i][j] == 0:
+                    nums = unusedNums(mat, i, j)
+                    if len(nums) == 1:
+                        mat[i][j] = nums[0]
+                        #print(i, j, nums[0])
+                    else:
+                        continue
+
+        for i in range(len(mat)):
+            print(mat[i])
+
+        print("-------")
+        filled(mat)
+
+
+def unusedNums(mat, i, j):
+    cellList = [1,2,3,4,5,6,7,8,9]
+    t = l = 0
+    if (i // 3) == 0:
+        t = 0
+    elif (i //3) == 1:
+        t = 3
+    else:
+        t = 6
+    if (j // 3) == 0:
+        l = 0
+    elif (j //3) == 1:
+        l = 3
+    else:
+        l = 6
+    # remove used nums in 3x3 cells
+    for x in range(t, t+3):
+        for y in range(l, l+3):
+            if mat[x][y] != 0:
+                if mat[x][y] in cellList:
+                    cellList.remove(mat[x][y])
+
+    #remove used nums in row
+    for y in range(len(mat)):
+        if mat[i][y] != 0:
+             if mat[i][y] in cellList:
+                 cellList.remove(mat[i][y])
+
+    #remove used nums in col
+    for x in range(len(mat)):
+        #print(i,y, mat[i][y])
+        if mat[x][j] != 0:
+             if mat[x][j] in cellList:
+                 cellList.remove(mat[x][j])
+    return cellList
+
+rows = collections.defaultdict(set)
+cols = collections.defaultdict(set)
+squares = collections.defaultdict(set)
 
 num = [1,2,3,4,5,6,7,8,9]
 
 n = numOfSpace(matrix)
-print(n)
+#print(n)
 
+r = 0
+c = 4
+unusedNums(matrix, r, c)
+filled(matrix)
 
 
 # def validSudo:
