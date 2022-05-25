7a7736b89d9a7002ccab7f35e6a0158d0de79c3b
cwzchan <cwchan0212@gmail.com>
2022-05-25 22:27:51 +0800

    Update case.py

--- case.py
+++ case.py
@@ -1,4 +1,6 @@
-# ans = [  [5,3,4,6,7,8,9,1,2]
+
+
+# ans = [     [5,3,4,6,7,8,9,1,2]
 #             [6,7,2,1,9,5,3,4,8]
 #             [1,9,8,3,4,2,5,6,7]
 #             [8,5,9,7,6,1,4,2,3]
@@ -9,8 +11,8 @@
 #             [3,4,5,2,8,6,1,7,9]
 #              ]
 
-
-question = [[5,3,0,0,7,0,0,0,0],
+import collections
+matrix = [  [5,3,0,0,7,0,0,0,0],
             [6,0,0,1,9,5,0,0,0],
             [0,9,8,0,0,0,0,6,0],
             [8,0,0,0,6,0,0,0,3],
@@ -21,11 +23,36 @@ question = [[5,3,0,0,7,0,0,0,0],
             [0,0,0,0,8,0,0,7,9]
             ]
 
-r = 9
-c = 9
-sq = [[0] *9 for _ in range(9)]
+def numOfSpace(mat):
+    space = 0;
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            if mat[i][j] == 0:
+                #print(mat[i][j])
+                space +=1
+            #sq[i][j] = question[i][j]
+    return space
+
+# rows = collections.defaultdict(set)
+# cols = collections.defaultdict(set)
+# squares = collections.defaultdict(set)
+
+num = [1,2,3,4,5,6,7,8,9]
+
+n = numOfSpace(matrix)
+print(n)
 
 
-for j in range(len(question)):
-    for i in range(len(question[j])):
-        sq[i][j] = question[i][j]
+
+# def validSudo:
+#     for c in range(9):
+#         for r in range(9):
+#             if matrix[r][c] == 0:
+#                 continue
+#             if (matrix[r][c] in rows[r] or matrix[r][c] in cols[c] or matrix[r][c] in squares[r//3, c//3]):
+#                 return False
+
+#             cols[c].add(matrix[r][c])
+#             rows[r].add(matrix[r][c])
+#             squares[r//3, c//3].add(matrix[r][c])
+#     return True
