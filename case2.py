5721d574277c55c6e3c381cd6c8b7ab5bb92dac7
cwzchan <cwchan0212@gmail.com>
2022-06-02 23:59:41 +0800

    Update case2.py

--- case2.py
+++ case2.py
@@ -14,17 +14,20 @@ class Sudoku:
         self.k = k
         self.sqn = math.sqrt(n)
         self.mat = [n][n]
-    #
-    # def fillBox(row, col):
-    #     num = 0;
-    #     for i in range(len(sqn)):
-    #         for j in range(len(sqn)):
+
+    def fillBox(row, col):
+        num = 0;
+        for i in range(len(sqn)):
+            for j in range(len(sqn)):
+                num = randomGenerator()
+                while (unUsedInBox(top, left, num) == True):
+                    mat[row+i][col+j] = num
 
     def randomGenerator(self) -> int:
         no = [1,2,3,4,5,6,7,8,9]
         return random.choice(no)
 
-    def unUsedInBox(self, top, left, num) -> bol:
+    def unUsedInBox(self, top, left, num) -> bool:
         for i in range(self.sqn):
             for j in range(self.sqn):
                 if (mat[top+i][left+i] == num):
