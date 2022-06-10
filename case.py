1eeabc6f41afead322f1f451f182db35ad96fd0c
cwzchan <cwchan0212@gmail.com>
2022-06-10 22:44:58 +0800

    Update case.py

--- case.py
+++ case.py
@@ -1,6 +1,7 @@
 from os import unsetenv
 import random
 import collections
+from typing import List
 
 class Sukodu:
 
@@ -52,6 +53,7 @@ class Sukodu:
 
     def filled(self):
         nums = []
+        zCnt  = 0
         space = self.spaceCount(self.matrix)
         fillcnt = 0
         if not self.validSudoku():
@@ -62,13 +64,18 @@ class Sukodu:
                 for j in range(len(self.matrix[i])):    # col
                     if self.matrix[i][j] == 0:
                         nums = self.unusedNums(i, j)
-                        if len(nums) == 1:
+                        if len(nums) > 0:
                             self.matrix[i][j] = nums[0]
 ##                            print("filled:", nums[0], "[", i+1, ",", j+1, "]")
                             fillcnt += 1
                             #print(i, j, nums[0])
                         else:
                             continue
+                zCnt = self.countRow(j)
+                if zCnt > 0:
+                    print(i,j)
+                    # swap
+                    return
 
             for i in range(len(self.matrix)):
                 print(self.matrix[i])
@@ -116,75 +123,225 @@ class Sukodu:
             if self.matrix[x][j] != 0:
                 if self.matrix[x][j] in cellList:
                     cellList.remove(self.matrix[x][j])
+
         return cellList
 
+    # def fillDiagonals(self):
+    #     for r in range(9):
+    #         for c in range(9):
+    #             if (r - c) == 0 or (r + c) == 8:
+    #                 if self.matrix[r][c] == 0: 
+    #                     num = self.unusedNums(r,c)
+    #                     if len(num) > 0:
+    #                         self.matrix[r][c] = random.choice(num)   
+    #                 else:
+    #                     continue                     
+
+    def fit(self, row: int):
+        cntZero = 0
+        cellList = []
+        unRow = []
+        cellListAddr = []
+
+        for r in range(row):
+            #cntZero = 0
+            for c in range(9):
+                if self.matrix[r][c] == 0: 
+                    num = self.unusedNums(r,c)
+                    if len(num) > 0:
+                        self.matrix[r][c] = random.choice(num)   
+            
+            cntZero = self.countRow(r)
+
+            print(cntZero)
+            if cntZero >= 0:
+                unRow = self.unUsedInRow(r)
+                cellList = self.unusedInRowGrid(r,c)
+                if len(unRow) and len(cellList):
+                    cellListAddr = self.cellListAddr(r, unRow[0], cellList)
+                    print(unRow, cellList, cellListAddr)
+                    #print(cellListAddr)
+            # #print(r, cntZero)
+            # if cntZero > 0: 
+            #     print("r", r, c, cntZero)
+            #     unRow = self.unUsedInRow(r)
+            #     cellList = self.unusedInRowGrid(r,c)
+            #     print("unRow", unRow, cellList) 
+            #     if unRow and cellList:
+            #         #print("unRow", unRow)                    
+            #         cellListAddr = self.cellListAddr(r, unRow[0], cellList)
+            #         print(cellListAddr)
+            #         # self.matrix[r][c] = num
+            #         # self.matrix[r][c], cellListAddr[0][0] = cellListAddr[0][0], self.matrix[r][c]
+            #     #print(l)
+            #     print("after")
+            #     for i in self.matrix:
+            #         print(i)
+            #     return
+
+
+    def countRow(self, row):
+        zcnt = 0
+        for r in range(row):
+            for c in range(9):
+                #print(r, c, self.matrix[row][c])
+                if self.matrix[row][c] == 0:
+                    zcnt += 1
+        return zcnt
+
+
+
+    def cellListAddr(self, row: int, num: int, cellList: List, ) -> dict:
+        
+        l = len(cellList)
+        cellAddr = []   
+        n = 0
+        for c in range(9):
+            if self.matrix[row][c] in cellList:
+                cellAddr.append([self.matrix[row][c], row,c])
+                n += 1
+        #print("cellListAddr", cellAddr)
+        return cellAddr
+
+    def unUsedInRow(self, row: int) -> List:
+        num = [1,2,3,4,5,6,7,8,9]
+        for c in range(9):
+            if self.matrix[row][c] in num:
+                num.remove(self.matrix[row][c])
+        return num
+
+    # try to fit unused in in celllist[0] to celllist[-1]
+    # check 3 x 3 valid: if yes, fit it
+    # 
+    #
+    #
+
+
+
+    def unusedInRowGrid(self, i, j):
+        rowList = [1,2,3,4,5,6,7,8,9]
+        cellList  = [1,2,3,4,5,6,7,8,9]
+        t = l = 0   # top & left
+        if (i // 3) == 0:
+            t = 0
+        elif (i //3) == 1:
+            t = 3
+        else:
+            t = 6
+        if (j // 3) == 0:
+            l = 0
+        elif (j //3) == 1:
+            l = 3
+        else:
+            l = 6
+        # for r in range(9):
+        #     for c in range(9):
+        #         if self.matrix[r][c] != 0:
+        #             cellList.remove(self.matrix[r][c])
+        for x in range(t, t+3):
+            for y in range(l, l+3):
+                if self.matrix[x][y] != 0:
+                    if self.matrix[x][y] in cellList:
+                        cellList.remove(self.matrix[x][y])
+        
+
+        
+        #print("cellList", cellList)
+        return cellList
+
+        # # remove used nums in 3x3 cells
+        # for x in range(t, t+3):
+        #     for y in range(l, l+3):
+        #         if self.matrix[x][y] != 0:
+        #             if self.matrix[x][y] in cellList:
+        #                 cellList.remove(self.matrix[x][y])
+
+        # #remove used nums in row
+        # for y in range(len(self.matrix)):
+        #     if self.matrix[i][y] != 0:
+        #         if self.matrix[i][y] in cellList:
+        #             cellList.remove(self.matrix[i][y])
+
+
+
+
+
+
+
     def genSudoku(self):
-        
         num1 = [1,2,3,4,5,6,7,8,9]
-        num2 = num1.copy()
-        num3 = num1.copy()
-        num4 = num1.copy()
-        #small = [[0] * 3 for _ in range(3)]
+        # self.fillDiagonals()
+        self.fit(2)
+        #self.filled()
+        print("\n")
+        for i in range(9):
+            print(self.matrix[i])
+
+        print("space:", self.spaceCount(self.matrix))
+        # num2 = num1.copy()
+        # num3 = num1.copy()
+        # num4 = num1.copy()
+        # #small = [[0] * 3 for _ in range(3)]
 
 
-        for c in range(3):
-            for r in range(3):
-                temp = random.choice(num1)
-                self.matrix[r][c] = temp
-                num1.remove(temp)
+        # for c in range(3):
+        #     for r in range(3):
+        #         temp = random.choice(num1)
+        #         self.matrix[r][c] = temp
+        #         num1.remove(temp)
 
-        for c in range(3,6):
-            for r in range(3,6):
-                temp = random.choice(num2)
-                self.matrix[r][c] = temp
-                num2.remove(temp)
+        # for c in range(3,6):
+        #     for r in range(3,6):
+        #         temp = random.choice(num2)
+        #         self.matrix[r][c] = temp
+        #         num2.remove(temp)
         
-        for c in range(6,9):
-            for r in range(6,9):
-                temp = random.choice(num3)
-                self.matrix[r][c] = temp
-                num3.remove(temp)
+        # for c in range(6,9):
+        #     for r in range(6,9):
+        #         temp = random.choice(num3)
+        #         self.matrix[r][c] = temp
+        #         num3.remove(temp)
 
-        for c in range(6,9):
-            for r in range(3):
-                temp = self.unusedNums(r,c)
-                if len(temp) > 0:
-                    n = random.choice(temp)
-                    self.matrix[r][c] = n
-                else:
-                    continue
-                #num4.remove(temp)    
+        # for c in range(6,9):
+        #     for r in range(3):
+        #         temp = self.unusedNums(r,c)
+        #         if len(temp) > 0:
+        #             n = random.choice(temp)
+        #             self.matrix[r][c] = n
+        #         else:
+        #             continue
+        #         #num4.remove(temp)    
 
-        for c in range(3):
-            for r in range(6,9):
-                temp = self.unusedNums(r,c)
-                if len(temp) > 0:
-                    n = random.choice(temp)
-                    self.matrix[r][c] = n
-                else:
-                    continue
+        # for c in range(3):
+        #     for r in range(6,9):
+        #         temp = self.unusedNums(r,c)
+        #         if len(temp) > 0:
+        #             n = random.choice(temp)
+        #             self.matrix[r][c] = n
+        #         else:
+        #             continue
 
-        for c in range(3,6):
-            for r in range(3):
-                temp = self.unusedNums(r,c)
-                if len(temp) > 0:
-                    n = random.choice(temp)
-                    self.matrix[r][c] = n
-                else:
-                    continue
+        # for c in range(3,6):
+        #     for r in range(3):
+        #         temp = self.unusedNums(r,c)
+        #         if len(temp) > 0:
+        #             n = random.choice(temp)
+        #             self.matrix[r][c] = n
+        #         else:
+        #             continue
 
-        for c in range(3,6):
-            for r in range(6,9):
-                temp = self.unusedNums(r,c)
-                if len(temp) > 0:
-                    n = random.choice(temp)
-                    self.matrix[r][c] = n
-                else:
-                    continue
+        # for c in range(3,6):
+        #     for r in range(6,9):
+        #         temp = self.unusedNums(r,c)
+        #         if len(temp) > 0:
+        #             n = random.choice(temp)
+        #             self.matrix[r][c] = n
+        #         else:
+        #             continue
 
-        self.filled()            
-        for i in self.matrix:
-            print(i , ",") 
+        # self.filled()            
+        # for i in self.matrix:
+        #     print(i , ",") 
 
 
         # t = l = 0   # top & left
@@ -242,6 +399,7 @@ class Sukodu:
 
 def action(n):
     # Generate new sudoku matrix
+    print(1)
     if n == 0:            
         matrix = [[0] * 9 for _ in range(9)]
         for i in range(9):
